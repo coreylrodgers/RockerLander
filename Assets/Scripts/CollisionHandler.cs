af2970f5690533e513d32a084253076bb3f697bf
Corey <corey.rodgers@droneshield.com>
2021-09-05 12:20:24 +1000

    add respawn on obstacle hit

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Hit friendly");
+                break;
+            case "Fuel":
+                Debug.Log("Hit fuel");
+                  break;
+            case "Finish":
+                Debug.Log("Congrats");
+                  break;
+            default:
+                Debug.Log("Hit an obstacle");
+           Respawn();
+                  break;
+        }
+    }
+
+    void Respawn() {
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
